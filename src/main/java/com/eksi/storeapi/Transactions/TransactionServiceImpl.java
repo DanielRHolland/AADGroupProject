941ded2c12cc1556ae46624f62d6173fd3e7eaff
Luke Barrett <lukejbarrett98@gmail.com>
2020-01-28 15:54:42 +0000

    Added GetTransactionLog functionality

--- src/main/java/com/eksi/storeapi/Transactions/TransactionServiceImpl.java
+++ src/main/java/com/eksi/storeapi/Transactions/TransactionServiceImpl.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
@@ -18,9 +19,10 @@ public class TransactionServiceImpl implements TransactionService {
         return tr.save(transaction);
     }
 
-    public List getTransactionLog(long dateFrom, long currentDate){
-        List<Transaction> transactionList = new ArrayList<>();
-
-        return transactionList;
+    public List<Transaction> getTransactionLog(long dateFrom, long currentDate){
+        List<Transaction> allTxs = (List<Transaction>) tr.findAll();
+        return allTxs.stream()
+                .filter(tx -> tx.getTimeStamp() > dateFrom && tx.getTimeStamp() < currentDate)
+                .collect(Collectors.toList());
     }
 }
