0aeee2489a7c6ee7922e8e57ca84cc64c3a35065
lukejbarrett98 <32837226+lukejbarrett98@users.noreply.github.com>
2020-02-04 13:29:55 +0000

    GetAllProducts function added under /products/l

--- src/main/java/com/eksi/storeapi/Products/ProductService.java
+++ src/main/java/com/eksi/storeapi/Products/ProductService.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.List;
 
 public interface ProductService {
-
     Product update(Product p) throws IOException;
     List getAllProducts();
     Product getProduct(Integer id);
