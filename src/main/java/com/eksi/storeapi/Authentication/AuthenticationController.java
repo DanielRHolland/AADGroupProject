4502223fa33c8ce93786ef7bb268e87d56dacca0
dan <me@danrh.co.uk>
2020-02-07 14:21:11 +0000

    REST enable CORS for /auth.

--- src/main/java/com/eksi/storeapi/Authentication/AuthenticationController.java
+++ src/main/java/com/eksi/storeapi/Authentication/AuthenticationController.java
@@ -1,12 +1,10 @@
 package com.eksi.storeapi.Authentication;
 
 import com.eksi.storeapi.ApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/auth")
 public class AuthenticationController {
     private ApiKeyService aks = ApplicationContext.apiKeyService();
