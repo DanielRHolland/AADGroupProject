33a50186f2b3b7a3dc566f9ba41abc81caff59b1
Luke Barrett <lukejbarrett98@gmail.com>
2020-02-11 15:54:50 +0000

    #92 - When saving, all transactions with the same ID will be deleted.

--- src/main/java/com/eksi/storeapi/Entries/EntriesServiceImpl.java
+++ src/main/java/com/eksi/storeapi/Entries/EntriesServiceImpl.java
@@ -13,6 +13,7 @@ public class EntriesServiceImpl implements EntriesService {
 
     @Override
     public List<Entries> update(List<Entries> entries) throws IOException {
+        deleteEntriesWithSameTransactionId(entries, entries.get(0).getTransactionId());
         for(Entries entry : entries){
             er.save(entry);
         }
@@ -22,6 +23,14 @@ public class EntriesServiceImpl implements EntriesService {
     @Override
     public List<Entries> getEntriesFromTransactionId(String id) {
         return er.getEntriesByTransactionId(id);
+    }
+
+    public void deleteEntriesWithSameTransactionId(List<Entries> entries, String transactionId){
+        for(Entries entry : entries){
+            if(entry.getTransactionId().equalsIgnoreCase(transactionId)){
+                er.delete(entry);
+            }
+        }
     }
 
 
